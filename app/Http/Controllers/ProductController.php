50fc306843f3ba5b25ce30863ad74664b7db8348
Iris1902 <irispicis1962@gmail.com>
2025-07-06 17:38:42 -0500

    :sparkles: feat: Add ProductController with store method for product creation

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json(['message' => 'Product created', 'data' => $product], 201);
+    }
+}
